d13af5c6b654ea67a08574163a62e99ca804665e
Missael Padilla <lmpadillar@gmail.com>
2022-09-17 05:31:26 -0700

    get user from data base

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [PagesComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule, HttpClientModule],
   exports: [PagesComponent],
 })
 export class PagesModule {}
